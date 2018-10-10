56f0494ad271b39c1590ea0f9805900f5555b03c
sr72703 <sr72703@sgh.waw.pl>
2018-10-10 13:08:55 +0200

    Adding argument to Main

--- src/pl/waw/sgh/Main.java
+++ src/pl/waw/sgh/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello!!!");
-        if (args.length>0) {
+        if (args.length > 1) {
                 System.out.println("Arg: " + args[0]);
         }
     }
